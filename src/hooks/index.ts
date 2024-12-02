45fb18713a895f1e534fa1652477484f36234ea5
gedion <117908662+Gedion-01@users.noreply.github.com>
2024-12-02 16:52:38 +0300

    added their respective export for both hooks & lib function

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+// Hooks: useFormattedDate, useFormattedDateTime, useFormattedEthiopianDate, useFormattedEthiopianDateTime
+export { useFormattedDate } from "./useFormattedDate";
+export { useFormattedDateTime } from "./useFormattedDateTime";
+export {
+  useFormattedEthiopianDate,
+  useFormattedEthiopianDateTime,
+} from "./useFormattedEthiopianDateTime";
---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+export { EthiopianDate } from "./ethiopian-date";
